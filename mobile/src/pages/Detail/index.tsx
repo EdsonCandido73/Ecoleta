539a9f767462c0111a1f90682659d2faea9afc19
Edson Candido <edson.cursoprogramacao@gmail.com>
2020-06-22 21:38:39 -0300

    Criando página do aplicativo, Pontos de Coleta e Detalhes do Ponto

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native';
-import { View, StyleSheet, Text, Image, TouchableOpacity, SafeAreaView } from 'react-native';
+import { View, StyleSheet, Text, Image, TouchableOpacity, SafeAreaView, Linking } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler'; 
+import * as MailComposer from 'expo-mail-composer';
 import api from '../../services/api';
 
 interface Params {
@@ -38,7 +39,18 @@ const Detail = () => {
 
 	function handleNavigateBack() {
 		navigation.goBack();
-	}
+  }
+
+  function handleWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}&text=Tenho interesse sobre coleta de resíduos`);
+  }
+
+  function handleComposeMail() {
+    MailComposer.composeAsync({
+      subject: 'Interesse na coleta de resíduos',
+      recipients: [data.point.email],
+    });
+  }
 	
 	if (!data.point) {
 		return null;
@@ -64,12 +76,12 @@ const Detail = () => {
 					</View>
 			</View>
 			<View style={styles.footer}>
-				<RectButton style={styles.button} onPress={() => {}} >
+				<RectButton style={styles.button} onPress={handleWhatsapp} >
 					<FontAwesome name="whatsapp" size={20} color="#FFF" />
 					<Text style={styles.buttonText}>Whatsapp</Text>
 				</RectButton>
 
-				<RectButton style={styles.button} onPress={() => {}} >
+				<RectButton style={styles.button} onPress={handleComposeMail} >
 					<Icon name="mail" size={20} color="#FFF" />
 					<Text style={styles.buttonText}>E-mail</Text>
 				</RectButton>
@@ -129,7 +141,6 @@ const styles = StyleSheet.create({
       borderTopWidth: StyleSheet.hairlineWidth,
       borderColor: '#999',
 			paddingVertical: 20,
-			paddingBottom: 10,
       paddingHorizontal: 32,
       flexDirection: 'row',
       justifyContent: 'space-between'
